0eb9367149c30b518a6fa0318c141c7dcdb958fa
naomi <orest.chupa@gmail.com>
2026-05-27 15:53:31 +0300

    Revert "Added Profile page, ability to modify user info at Profile page"

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from task_manager.views import IndexView, WorkerDetailView, WorkerUpdateFieldView
+from task_manager.views import IndexView
 
 app_name = "task_manager"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
-    path("workers/<int:pk>/update/<str:field>/", WorkerUpdateFieldView.as_view(), name="worker-update-field"),
 ]
